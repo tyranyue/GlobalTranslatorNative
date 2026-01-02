c308ffc0b862177da4ece667b7e4b0fcefd27714
tyranyue <imtyranyue@icloud.com>
2026-01-02 17:14:32 +0800

    Initial Commit

---
+++ GlobalTranslatorNative/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlobalTranslatorNative
+//
+//  Created by 岳展辉 on 2026/1/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlobalTranslatorNative/GlobalTranslatorNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalTranslatorNativeApp.swift
+//  GlobalTranslatorNative
+//
+//  Created by 岳展辉 on 2026/1/2.
+//
+
+import SwiftUI
+
+@main
+struct GlobalTranslatorNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
